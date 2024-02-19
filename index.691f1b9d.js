b756de96bd93b3c3150c7a8e1e9e9ce60287f8a6
turbomatrixxxl <turbomatrixxxl@gmail.com>
2024-02-19 09:03:06 +0000

    Deploying to gh-pages from @ turbomatrixxxl/primul-proiect-de-grup@c1ce40d2b364affc008dcbed8c51ed80934643e0 🚀

---
+++ index.691f1b9d.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelector(".js-menu-container"),n=document.querySelector(".js-open-menu"),t=document.querySelector(".js-close-menu"),c=()=>{e.classList.toggle("is-open")};n.addEventListener("click",c),t.addEventListener("click",c)}));
+//# sourceMappingURL=index.691f1b9d.js.map
