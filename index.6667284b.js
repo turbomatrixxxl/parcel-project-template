c3fabc0be52932047865c22a293cceac780d5af6
turbomatrixxxl <turbomatrixxxl@gmail.com>
2024-02-19 19:03:27 +0000

    Deploying to gh-pages from @ turbomatrixxxl/primul-proiect-de-grup@c16cbaa1cf9842f1fc6300a0ebfdbf2d82467aae 🚀

--- index.6667284b.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".js-menu-container"),n=document.querySelector(".js-open-menu"),t=document.querySelector(".js-close-menu"),c=function(){e.classList.toggle("is-open")};n.addEventListener("click",c),t.addEventListener("click",c)}));
-//# sourceMappingURL=index.6667284b.js.map
